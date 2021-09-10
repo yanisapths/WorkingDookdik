9ac3751322d8c90951dfa742c91f7100f716be08
yanisapths <72002605+yanisapths@users.noreply.github.com>
2021-09-10 12:30:49 +0700

    Edited Slide in Works

--- src/data.js
+++ src/data.js
@@ -12,7 +12,7 @@ export const projectsPortfolio = [
     {
       id: 1,
       title: "Web Social Media App",
-      img: "https://images.unsplash.com/photo-1630797160655-35e602b644c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+      img: "https://images.unsplash.com/photo-1631005258849-aee1ccce98f7?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNjZ8fHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
     }
   ];
   
